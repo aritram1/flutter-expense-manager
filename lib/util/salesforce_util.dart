fb18461abb1840ff2f4bdb13788207123d9d4d07
aritra mukherjee <aritramukherjee@aritras-MacBook-Air.local>
2023-12-01 03:46:46 +0530

    Changes in dev branch latest

--- lib/util/salesforce_util.dart
+++ lib/util/salesforce_util.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'dart:core';
 import 'package:flutter_sms_inbox/flutter_sms_inbox.dart';
 import 'package:http/http.dart' as http;
+import 'package:device_info/device_info.dart';
 import 'message_util.dart'; // own lib file
 
 class SalesforceUtil {
@@ -17,6 +18,7 @@ class SalesforceUtil {
   final String _userName = 'aritram1@gmail.com.financeplanner'; 
   final String _tokenGrantType = 'password'; 
   final String _pwdWithToken = 'financeplanner123W8oC4taee0H2GzxVbAqfVB14'; 
+  final int CONST_BATCH_SIZE = 200;
   String instanceUrl = '';
   String token = '';
   DateTime? tokenExpiresAt;
@@ -60,19 +62,37 @@ class SalesforceUtil {
   ///////////////////////////////Save To Salesforce////////////////////////////////////
   /////////////////////////////////////////////////////////////////////////////////////
   Future<String> saveToSalesForce(String sender, int count) async {
+    
+    String result = '';
+    List<SmsMessage> eachList = [];
+    int counter = 0;
+
     allMessages.clear();
     allMessages = await MessageUtil().getMessages(sender, count); // '' = get all messages
-    for(List<SmsMessage> sms in allMessages){
-      saveToSalesForceEach(sender, count);
+      
+    for(SmsMessage sms in allMessages){
+      eachList.add(sms);
+      counter++;
+      if(counter == CONST_BATCH_SIZE){
+        String currentResult = await saveEachListToSalesForce('', 0, eachList);
+        print('currentresult=>$currentResult');
+        result += currentResult;
+        counter = 0;
+        eachList = [];
+      }     
     }
+    return result;
   }
+
   /////////////////////////////////////////////////////////////////////////////////////
   ///////////////////////////////Save To Salesforce////////////////////////////////////
   /////////////////////////////////////////////////////////////////////////////////////
-  Future<String> saveToSalesForceEach(String sender, int count, List<SmsMessage> messages) async {
+  Future<String> saveEachListToSalesForce(String sender, int count, List<SmsMessage> messages) async {
     
     String sfResponse = '';
-    
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+    String deviceName = androidInfo.model;
     // Generate the token and retrieve the messages if not already done
     if(token == '' || instanceUrl == '') await loginToSalesforce();
     
@@ -93,7 +113,8 @@ class SalesforceUtil {
         },
         "FinPlan__Content__c": "${sms.body != null && sms.body!.length > 255 ? sms.body?.substring(0, 255) : sms.body}",
         "FinPlan__Sender__c": "${sms.sender}",
-        "FinPlan__Received_At__c": sms.date.toString()
+        "FinPlan__Received_At__c": sms.date.toString(),
+        "FinPlan__Device__c": deviceName
       };
       //print('record is $record');
       allRecords.add(record);
@@ -105,17 +126,17 @@ class SalesforceUtil {
     //print('Body is ${jsonEncode(requestBody)}');
     try{
         final response = await http.post(
-        Uri.parse('$instanceUrl/services/data/v53.0/composite/tree/FinPlan__SMS_Message__c'),
-        headers: headers,
-        body: jsonEncode(requestBody)
-      );
-      //print('Response received as ${response.statusCode}');
-      if (response.statusCode == 201) { 
-        final Map<String, dynamic> data = json.decode(response.body);
-        sfResponse = data.toString();
-      } else {
-        sfResponse = CONST_SAVE_SMS_ERROR;      
-      }
+          Uri.parse('$instanceUrl/services/data/v53.0/composite/tree/FinPlan__SMS_Message__c'),
+          headers: headers,
+          body: jsonEncode(requestBody)
+        );
+        //print('Response received as ${response.statusCode}');
+        if (response.statusCode == 201) { 
+          final Map<String, dynamic> data = json.decode(response.body);
+          sfResponse = data.toString();
+        } else {
+          sfResponse = response.body;//statusCode.toString();//CONST_SAVE_SMS_ERROR;      
+        }
     }
     on Exception catch (_, e){
       sfResponse = e.toString();
