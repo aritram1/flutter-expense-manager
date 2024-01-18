1d21a7bf116c3836b4720600fce22c292a371bd1
aritra mukherjee <aritramukherjee@aritras-MacBook-Air.local>
2024-01-18 23:31:34 +0530

    Changes

--- lib/widgets/util.dart
+++ lib/widgets/util.dart
@@ -71,4 +71,45 @@ class Util {
     return mesageAndTransactionsDeleteMessage;
   }
 
+  static Future<List<dynamic>> getInvestmentsData() async{
+    
+    var investmentList = [];
+
+    Map<String, dynamic> response = await SalesforceUtil.queryFromSalesforce(
+      objAPIName: 'FinPlan__Investment__c', 
+      fieldList: ['Id', 'Name', 'FinPlan__Type__c', 'FinPlan__Investment_Code__c'], 
+      whereClause: "FinPlan__Status__c = 'Active'",
+      orderByClause: 'FinPlan__Type__c asc, Name asc',
+    );
+
+    dynamic error = response['error'];
+    dynamic data = response['data'];
+
+    if(debug) log.d('Error inside getInvestmentsList : ${error.toString()}');
+    if(debug) log.d('Data inside getInvestmentsList : ${data.toString()}');
+    
+    if(error != null && error.isNotEmpty){
+      if(debug) log.d('Error occurred while querying inside getInvestmentsList : ${response['error']}');
+      //return null;
+    }
+    else if (data != null && data.isNotEmpty) {
+      try{
+        if(detaildebug) log.d('Inside getInvestmentsList Data where data is not empty');
+        dynamic records = data['data'];
+        if(records != null && records.isNotEmpty){
+          for (var record in records) {
+            Map<String, dynamic> recordMap = Map.castFrom(record);
+            investmentList.add(record);
+          }
+        }
+      }
+      catch(error){
+        if(debug) log.e('Error Inside getInvestmentsList : $error');
+      }
+    }
+    if(debug) log.d('Inside getInvestmentsList=>$investmentList');
+    return investmentList; 
+
+  }
+
 }
